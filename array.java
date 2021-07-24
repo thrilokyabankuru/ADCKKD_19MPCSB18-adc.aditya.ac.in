085896858814bad3beb3ae92e827b64323fa5ad0
thrilokyabankuru <86638912+thrilokyabankuru@users.noreply.github.com>
2021-07-24 20:03:22 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class array
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+for(int j=0;j<n;j++)
+a[j]=s.nextInt();
+int target=s.nextInt();
+int result=0;
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]==target)   //1 3 5 7
+            {
+                result=i;
+                System.out.println(result); 
+                break;
+            }
+            else if(a.length==1 && target!=0)
+            {
+                result=1;
+                System.out.println(result); 
+                break;
+            }  
+            else if(a[i]<target && a[i+1]>target)
+            {
+                result=i+1;
+               System.out.println(result);
+               break;
+            }
+            else if(a[i]<target && a[n-1]<target)
+            {
+                result=n;
+               System.out.println(n);
+               break;
+               
+            }
+            else if(target==0)
+            {
+                result=0;
+               System.out.println(result);
+               break;
+              
+            }
+        }
+        
+    }
+}

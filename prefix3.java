1129bb1a60a4140294ec3659a9361546acc4214d
thrilokyabankuru <86638912+thrilokyabankuru@users.noreply.github.com>
2021-08-02 16:57:32 +0530

    Add files via upload

---
+++ prefix3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class prefix3
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int a[]=new int[n];
+a[0]=1;
+for(int i=1;i<n;i++)
+{
+a[i]=a[i-1]*i;
+}
+int fact=s.nextInt();
+System.out.println(a[fact]);
+}
+}

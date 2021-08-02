0c159bd86034eacc7239bd77178ab0b103a16e6f
thrilokyabankuru <86638912+thrilokyabankuru@users.noreply.github.com>
2021-08-02 17:50:54 +0530

    Add files via upload

--- prefix3.java
+++ prefix3.java
@@ -4,6 +4,7 @@ class prefix3
 public static void main(String args[])
 {
 Scanner s=new Scanner(System.in);
+System.out.println("enter n value");
 int n=s.nextInt();
 int a[]=new int[n];
 a[0]=1;
@@ -11,7 +12,12 @@ for(int i=1;i<n;i++)
 {
 a[i]=a[i-1]*i;
 }
+System.out.println("Enter Query");
+int Q=s.nextInt();
+for(int j=0;j<Q;j++)
+{
 int fact=s.nextInt();
 System.out.println(a[fact]);
+}
 }
 }

cc43664d6d98f5986cdc7a44998fe06faae2c170
thrilokyabankuru <86638912+thrilokyabankuru@users.noreply.github.com>
2021-08-14 19:01:21 +0530

    Update pstring1.java

--- pstring1.java
+++ pstring1.java
@@ -14,8 +14,33 @@ cba  */
 import java.util.*;
 class pstring
 {
-public static void main(String args])
+public static void main(String args[])
 {
-s
+Scanner sc=new Scanner(System.in);
+String s= sc.next();
+permutation(s,"");
+}
+
+public static void permutation(String input,String output) // ""
+{
+if(input.length()==0)                         
+{                                               
+System.out.println(output);                      
+return;                                         
+}                                               
+else                                             
+{ 
+for(int i=0;i<input.length();i++)
+{ 
+char ch = input.charAt(i); 
+String left = input.substring(0,i); 
+String right = input.substring(i+1);
+String input1=left+right; 
+String output1 = output+ ch; 
+permutation(input1,output1); 
+}
+}
+}
+}
 
 

ad89b99651ef1277aa8c1ca66796e9f0dc6d146b
thrilokyabankuru <86638912+thrilokyabankuru@users.noreply.github.com>
2021-08-14 18:59:55 +0530

    Add files via upload

---
+++ pstring1.java
@@ -0,0 +1,21 @@
+/* Write a java program to generate substrings for given string 
+"abc" using recursive permutations:
+Example:
+Input: abc
+Output:
+Possible Substrings are:
+abc
+acb
+bac
+bca
+cab
+cba  */
+
+import java.util.*;
+class pstring
+{
+public static void main(String args])
+{
+s
+
+
